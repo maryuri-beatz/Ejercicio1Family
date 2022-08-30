a7ba0242e89535a0c900e1f0e0adb0e143a16f5f
maryuri-beatz <maryuribeatrizgarcia@gmail.com>
2022-08-30 10:30:56 -0600

    primer commit

---
+++ src/variable/numerico/EjercicioFamilia.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package variable.numerico
+
+/**
+ *
+ * @author maryuri
+ */
+public class EjercicioFamilia {
+    public static void main (String[]args){
+    var family = 8;
+    System.out.println("mi mama es: Yanira Francisca Garcia de Santana");
+    System.out.println("mi padre es: Jose Galileo Santana Elias");
+    System.out.println("mi hermana es: Kenia Adelayda Santana de Venitez");
+    System.out.println("mi cuñado es: Alvaro Andres Venitez Vazques");
+    System.out.println("mi abuela es: Enohemi de Paz ");
+    System.out.println("mi abuelo es: Jose Santana de Paz");
+    System.out.println("en total mi family es");
+    System.out.println(family);
+    }
+    
+}
